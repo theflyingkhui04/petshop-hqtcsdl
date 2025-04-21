cc07a57fedf19ff366a6f87b79b4aa97c4165fcf
Can Duc Khoi <133144430+theflyingkhui04@users.noreply.github.com>
2025-04-21 14:47:20 +0700

    Create JanyPetApplicationTests.java

---
+++ src/test/java/com/BTL_LTW/JanyPet/JanyPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BTL_LTW.JanyPet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanyPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
